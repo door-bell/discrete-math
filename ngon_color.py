c6218c950e7f9644e45ba54dc6c7edf9724fead7
Tim Beck <socaltdb@gmail.com>
2019-11-12 09:14:28 -0800

    Chagne driver code to have better output

--- ngon_color.py
+++ ngon_color.py
@@ -15,12 +15,16 @@ def backtrack(ngon, colors, current, result):
             ngon[current] = i
             backtrack(ngon, colors, current + 1, result)
             ngon[current] = None
-     
+
 def isSafe(ngon, index, color):
+    not_prev_color = ngon[index - 1] != color
     if index == len(ngon) - 1:
-        return ngon[index - 1] != color and ngon[0] != color
-    return ngon[index - 1] != color and ngon[index + 1] != color
+        return not_prev_color and ngon[0] != color
+    return not_prev_color and ngon[index + 1] != color
 
 if __name__ == "__main__":
     # Can also simply print countWays(n, k) to see each configuration.
-    print(3, len(countWays(5, 4)))
+    ways = countWays(5, 4)
+    for way in ways:
+        print(way)
+    print(len(ways))
